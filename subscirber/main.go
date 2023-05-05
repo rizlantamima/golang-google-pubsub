a13cbb934b2bf5cfdadedccb09bb808f6b79941c
Rizlan Tamima <rizlan@ojekbro.com>
2023-05-05 16:11:13 +0700

    fixing subscriber more interactive

--- subscirber/main.go
+++ subscirber/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"sync/atomic"
 
 	"cloud.google.com/go/pubsub"
 	"github.com/joho/godotenv"
+	"google.golang.org/api/iterator"
 )
 
 func main() {
@@ -22,7 +24,6 @@ func main() {
 	}
 
 	projectID := os.Getenv("GOOGLE_PROJECT_ID")
-	subID := os.Getenv("GOOGLE_PUBSUB_SUBSCRIPTION_ID")
 
 	pubsubClient, err := pubsub.NewClient(ctx, projectID)
 	if err != nil {
@@ -30,11 +31,56 @@ func main() {
 		panic(err)
 	}
 	defer pubsubClient.Close()
-	stream(ctx, pubsubClient, subID)
+
+	subs, err := listSubscriptions(ctx, pubsubClient)
+	if err != nil {
+		err = fmt.Errorf("pubsub: NewClient: %v", err)
+		panic(err)
+	}
+	fmt.Printf("\nList of available subscriptions on your project :\n\n")
+
+	for key, value := range subs {
+		fmt.Println("  ", key+1, value)
+	}
+
+	var subChoosenNumber int
+	fmt.Printf("\n\nplease select one of the subscriptions numbers above : 1 - %v \n", len(subs))
+	_, err = fmt.Scan(&subChoosenNumber)
+	if err != nil {
+		panic(err)
+	}
+
+	if subChoosenNumber > len(subs) {
+		err := fmt.Errorf("you are choosing invalid / unavailable number")
+		panic(err)
+	}
+
+	subChoosen := subs[subChoosenNumber-1]
+
+	stream(ctx, pubsubClient, subChoosen)
+}
+
+func listSubscriptions(ctx context.Context, client *pubsub.Client) (subs []string, err error) {
+
+	it := client.Subscriptions(ctx)
+	for {
+		s, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("Next: %v", err)
+		}
+
+		subNameArr := strings.Split(s.String(), "/")
+		subName := subNameArr[len(subNameArr)-1]
+		subs = append(subs, subName)
+	}
+	return subs, nil
 }
 
 func stream(ctx context.Context, client *pubsub.Client, subID string) error {
-
+	fmt.Printf("\n subscribing message from %s ... \n \n", subID)
 	sub := client.Subscription(subID)
 
 	// Receive messages for 10 seconds, which simplifies testing.
@@ -55,7 +101,7 @@ func stream(ctx context.Context, client *pubsub.Client, subID string) error {
 
 func pullHandler(_ context.Context, msg *pubsub.Message) {
 	var received int32
-	fmt.Printf("Got message: %q\n", string(msg.Data))
+	log.Printf("\n Got message: id(%s) %q, this message are published at %s \n", msg.ID, string(msg.Data), msg.PublishTime)
 	atomic.AddInt32(&received, 1)
 	msg.Ack()
 }
